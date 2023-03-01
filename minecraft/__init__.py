0f0f1acd0eb1c767deb00de758fbbcffd7304298
plun1331 <49261529+plun1331@users.noreply.github.com>
2023-03-01 20:06:25 +0000

    chore: update license

--- minecraft/__init__.py
+++ minecraft/__init__.py
@@ -1,23 +1,11 @@
 """
-Copyright (c) 2023 plun1331
+MCPy
+~~~~~
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+A Python library for Minecraft clients.
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+:copyright: (c) 2023-present plun1331
+:license: BSD 3-Clause, see LICENSE for more details.
 """
 
 from .datatypes import *
